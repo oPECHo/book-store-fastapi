258f683362dfc404d2f013991280ac399e1ec376
ณัฐนนท์ ชุมฤทธิ์ <nattanon0280@gmail.com>
2023-03-30 21:43:49 +0700

    connect Frontend

--- book-server/main.py
+++ book-server/main.py
@@ -2,7 +2,21 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import book, category
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(engine)
 
